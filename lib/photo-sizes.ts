c5d00e19c1b8ff43a2583ec3567d6151e982eca4
yanchengtian <yanchengtian@popmart.com>
2026-04-07 14:47:55 +0800

    完善代码

--- lib/photo-sizes.ts
+++ lib/photo-sizes.ts
@@ -267,7 +267,7 @@ export function getPhotoSizeById(id: string): PhotoSize | undefined {
  */
 const DEFAULT_CROP_CONFIG: CropStyleConfig = {
   defaultMode: 'lomo',
-  availableModes: [ 'cover','lomo']
+  availableModes: [ 'cover','lomo', 'full']
 }
 
 /**
